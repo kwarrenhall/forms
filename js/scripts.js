18576a794d6a2f21eb6120662dec04c6b208ad24
Kate Hall and lindsey raffa <kwarrenhall@gmail.com>
2016-01-12 14:28:54 -0800

    initial commit mad libs form

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
-  $("button#Meow").click(function() {
-    $("ul#cat").prepend("<li>Meow!</li>");
-  //   $("ul#dog").prepend("<li>Roof!</li>");
-  //
+  $("#blanks form").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
   });
-
-  $("button#Roof").click(function() {
-    $("ul#dog").prepend("<li>Roof!</li>");
-    // $("ul#cat").prepend("<li>Meow!</li>");
-
-});
-
 });
