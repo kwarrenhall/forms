3b06add83ec5e20200dceaab48e7953fc8149da1
Kate Hall and lindsey raffa <student@epicodus.com>
2016-01-12 11:07:02 -0800

    cat and dogs barking first step

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("button#Meow").click(function() {
+    $("ul").prepend("<li>Meow!</li>");
+
+  });
+
+  $("button#Roof").click(function() {
+    $("ul").prepend("<li>roof!</li>");
+
+});
+
+});
