3246c8b7a8a215842e809ab429c02996592d0cf8
Kate Hall and lindsey raffa <lindsey.raffa@gmail.com>
2016-01-12 15:06:01 -0800

    new form-portland booking info

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var date1Input = $("input#date1").val();
+    var date2Input= $("input#date2").val();
+    var requestsInput = $("input#requests").val();
 
     $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".date1").text(date1Input);
+    $(".date2").text(date2Input);
+    $(".requests").text(requestsInput);
+
 
     $("#story").show();
 
